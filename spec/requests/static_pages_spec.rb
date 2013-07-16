181b2d65edf959d4e17922aad0e52700974cecdb
courte <courteney.ervin@gmail.com>
2013-07-16 10:56:39 -0400

    Finish static pages: home, help, about & contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,24 +5,55 @@ describe "StaticPages" do
   describe "Home page" do
     
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
+      visit '/static_pages/home/'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/home/'
+      page.should have_selector('title',
+            :text => " | Home")
     end
   end
 
   describe "Help page" do
 
   	it "should have the content 'Help'" do
-  	  visit '/static_pages/help'
-  	  page.should have_content ('Help')
-  	end 
+  	  visit '/static_pages/help/'
+  	  page.should have_selector('h1', :text => 'Help')
+  	end
+
+    it "should have the right title" do
+      visit '/static_pages/help/'
+      page.should have_selector('title',
+            :text => " | Help")
+    end 
   end
 
   describe "About page" do
 
   	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
-  		page.should have_content ('About Us')
+  		visit '/static_pages/about/'
+  		page.should have_selector('h1', :text => 'About Us')
   	end
+
+    it "should have the right title" do
+      visit '/static_pages/about/'
+      page.should have_selector('title',
+            :text => " | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact/'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact/'
+      page.should have_selector('title', :text => ' | Contact Us')
+    end
   end
 end
