0142cc6c8588a5b8cf9bfc520029882997499364
courte <courteney.ervin@gmail.com>
2013-07-18 11:22:24 -0400

    Streamline spec tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,11 +1,4 @@
-def full_title(page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
+include ApplicationHelper
 
 RSpec::Matchers::define :have_title do |text|
   match do |page|
