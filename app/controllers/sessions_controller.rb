3dc0d97cde1299b4b2e7242b53333dc535f0f1c1
courte <courteney.ervin@gmail.com>
2013-07-27 11:54:13 -0400

    Develop signin form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by_email(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# Sign the user in and redirect to the user's show page
+		else
+			# Create an error message and re-render the signin form
+			render 'new'
+		end
+	end
+
+	def destroy
+	end
 end
