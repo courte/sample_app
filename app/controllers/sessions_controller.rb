b3042a977d11ab3b20800ef56f6821943212370b
courte <courteney.ervin@gmail.com>
2013-07-27 18:07:50 -0400

    Complete signin action with invalid information

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by_email(email: params[:session][:email].downcase)
+		user = User.find_by_email(params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			# Sign the user in and redirect to the user's show page
+			redirect_to user_path(user)
 		else
-			# Create an error message and re-render the signin form
+			flash.now[:error] = 'Invalid email/password combination' #Not quite right!
 			render 'new'
 		end
 	end
